62e96c3c3bd60579ed5acf46651440e4a2bc3edb
Johann Dickson <johann.dickson@springload.co.nz>
2021-08-03 11:02:39 +1200

    Export names

--- outputs.tf
+++ outputs.tf
@@ -2,6 +2,14 @@ output "cluster_arn" {
   value = aws_ecs_cluster.main.arn
 }
 
+output "cluster_name" {
+  value = aws_ecs_cluster.main.name
+}
+
 output "asg_arn" {
   value = var.instances_desired > 0 ? aws_autoscaling_group.ASG[0].arn : null
+}
+
+output "asg_name" {
+  value = var.instances_desired > 0 ? aws_autoscaling_group.ASG[0].name : null
 }
