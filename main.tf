24b983f97709a4ab17dfed72c025b162b0b113d3
Andrei Vsiakikh <andrei.vsiakikh@springload.co.nz>
2024-04-24 10:01:29 +1200

    ecs_cluster_loggin

--- main.tf
+++ main.tf
@@ -148,7 +148,6 @@ resource "aws_ecs_cluster" "main" {
         logging    = "OVERRIDE"
 
         log_configuration {
-          cloud_watch_encryption_enabled = true
           cloud_watch_log_group_name     = aws_cloudwatch_log_group.ecs_exec_logs[0].name
         }
       }
